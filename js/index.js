308afbc13157cc20790cad43912587df40ad5abe
DevUser <webdevelopment.career@gmail.com>
2019-09-19 22:45:52 -0300

    container fix

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import {getData} from './connection.js';
 const url = ('https://jsonplaceholder.typicode.com/posts');
 
 var container = document.createElement('div');
-    container.setAttribute('class', 'container-fluid m-3 p-3 bg-dark jumbotron');
+    container.setAttribute('class', 'container m-3 p-3 bg-dark jumbotron');
 
 getData(url, container);
 
